5feb247867470b2d86cbfebe3a15fde54fd5d7d6
fazlerabbytalukder <talukderfazlerabby@gmail.com>
2022-04-19 20:05:22 +0600

    title button

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Carousel, Col, Container, FormControl, InputGroup, Row } from 'react-bootstrap';
+import { Carousel } from 'react-bootstrap';
 import './Banner.css';
 import bannerImg1 from '../../../images/banner/banner-image-1.jpg';
 import bannerImg2 from '../../../images/banner/banner-image-2.jpg';
@@ -14,64 +14,44 @@ const Banner = () => {
                 <Carousel fade>
                     <Carousel.Item >
                         <div className='carsuel-items'>
-                        <img
-                            className="d-block w-100 banner-img"
-                            src={bannerImg2}
-                            alt="First slide"
+                            <img
+                                className="d-block w-100 banner-img"
+                                src={bannerImg2}
+                                alt="First slide"
                             />
                             <div className="overlay"></div>
                         </div>
                         <Carousel.Caption className='carusel-cations'>
                             <h5>Our Park Choose A Holiday Home</h5>
-                            <h1 className='carusel-header'>A Full Day Of Fun For The <br /> Entire Family</h1>
-                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus dignissimos illo voluptatibus dolor optio laboriosam sit recusandae iste porro molestiae.</p>
-                            <button>GET STARTED</button>
-                            <button>CONTACT US</button>
+                            <h1 className='carusel-header extra-bold'>A Full Day Of Fun For The <br /> Entire Family</h1>
+                            <p className='carusel-paragraph'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus dignissimos illo voluptatibus dolor optio laboriosam sit recusandae iste porro molestiae.</p>
+                            <button className='button-design-2 me-3'>GET STARTED</button>
+                            <button className='button-design-1'>CONTACT US</button>
                         </Carousel.Caption>
-                        
+
                     </Carousel.Item>
 
 
                     <Carousel.Item>
-                    <div className='carsuel-items'>
-                        <img
-                            className="d-block w-100 banner-img"
-                            src={bannerImg1}
-                            alt="First slide"
+                        <div className='carsuel-items'>
+                            <img
+                                className="d-block w-100 banner-img"
+                                src={bannerImg1}
+                                alt="First slide"
                             />
                             <div className="overlay"></div>
                         </div>
 
                         <Carousel.Caption className='carusel-cations'>
-                        <h5>Our Park Choose A Holiday Home</h5>
-                            <h1 className='carusel-header'>We Offer Premier <br /> Holiday Attractions</h1>
-                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus dignissimos illo voluptatibus dolor optio laboriosam sit recusandae iste porro molestiae.</p>
-                            <button>GET STARTED</button>
-                            <button>CONTACT US</button>
+                            <h5>Our Park Choose A Holiday Home</h5>
+                            <h1 className='carusel-header extra-bold'>We Offer Premier <br /> Holiday Attractions</h1>
+                            <p className='carusel-paragraph'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus dignissimos illo voluptatibus dolor optio laboriosam sit recusandae iste porro molestiae.</p>
+                            <button className='button-design-2 me-3'>GET STARTED</button>
+                            <button className='button-design-1'>CONTACT US</button>
                         </Carousel.Caption>
                     </Carousel.Item>
                 </Carousel>
             </div>
-            {/* <Container className='banner-text'>
-                <Row className='d-flex justify-content-between align-items-center'>
-                    <Col>
-                        <img className='img-fluid' src={banner1} alt="" />
-                    </Col>
-                    <Col>
-                        <h2 className='sub-heading mt-3 '>Park Lover?</h2>
-                        <h1 className='heading  fw-bold'>Find Here Best Park</h1>
-                        <h5 className='slogan mb-4 '>Please Book Our Services</h5>
-                        <InputGroup className="mb-3 w-50">
-                            <FormControl
-                                placeholder="Service Name"
-                                aria-label="Service Name"
-                                aria-describedby="basic-addon2"
-                            />
-                            <InputGroup.Text id="basic-addon2">Search</InputGroup.Text>
-                        </InputGroup>
-                    </Col>
-                </Row>
-            </Container> */}
         </div>
     );
 };
