74a3c1021e79ed49077f874caea079dab448e0d8
fazlerabbytalukder <talukderfazlerabby@gmail.com>
2022-04-21 16:34:17 +0600

    contact design finish

--- src/Pages/Home/ContactUs/ContactUs.js
+++ src/Pages/Home/ContactUs/ContactUs.js
@@ -9,7 +9,7 @@ var sectionStyle = {
     backgroundImage: `url(${contactImg})`,
     overFlow: 'hidden',
     width: '100%',
-    height:'auto'
+    height: 'auto'
 };
 
 const ContactUs = () => {
@@ -23,7 +23,7 @@ const ContactUs = () => {
         <div>
             <div style={sectionStyle} id='contact'>
                 <div>
-                <div className=' d-flex flex-column justify-content-center align-items-center'>
+                    <div className=' d-flex flex-column justify-content-center align-items-center'>
                         <h1 style={{ color: 'white' }} className='fw-bold title mt-3'>Contact Us</h1>
                         <img src={titleImg} alt="" />
                     </div>
@@ -33,44 +33,16 @@ const ContactUs = () => {
                 </div>
             </div >
             <div className='container mt-5'>
-            <div className="row text-center">
-                    <div className="col-md-6">
-                        <div>
-                            <div className="card card-background mb-3">
-                                <div className="card-body">
-                                    <i className="fa-solid fa-envelope"></i>
-                                    <h5>Email</h5>
-                                    <p>admin@tangaildistrictpark.com</p>
-                                    <a href="mailto:talukderfazlerabby@gmail.com" target="_blank" rel="noreferrer">Send a message</a>
-                                </div>
-                            </div>
-                            <div className="card card-background mb-3">
-                                <div className="card-body">
-                                    <i className="fa-brands fa-facebook-messenger"></i>
-                                    <h5>Messanger</h5>
-                                    <p>TD Park</p>
-                                    <a href="https://m.me/frtfazlerabby" target="_blank" rel="noreferrer">Send a message</a>
-                                </div>
-                            </div>
-                            <div className="card card-background mb-3">
-                                <div className="card-body">
-                                    <i className="fa-brands fa-whatsapp"></i>
-                                    <h5>WhatsApp</h5>
-                                    <p>+8801739719796</p>
-                                    <a href="https://wa.me/8801739719796" target="_blank" rel="noreferrer">Send a message</a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className="col-md-6 text-start">
+                <div className="row text-center">
+                    <div className="col-md-12 mb-5 text-start">
                         <form ref={form} onSubmit={sendEmail} className='all-inputs'>
                             <div className="mb-3">
                                 <label for="exampleFormControlInput1" className="form-label">Your Name</label>
-                                <input type="text" name='name' className="form-control from-design" id="exampleFormControlInput1" placeholder="Mr.X" />
+                                <input type="text" name='name' className="form-control from-design w-50" id="exampleFormControlInput1" placeholder="Mr.X" />
                             </div>
                             <div className="mb-3">
                                 <label for="exampleFormControlInput1" className="form-label">Your Email</label>
-                                <input type="email" name='email' className="form-control from-design" id="exampleFormControlInput2" placeholder="name@example.com" />
+                                <input type="email" name='email' className="form-control from-design w-50" id="exampleFormControlInput2" placeholder="name@example.com" />
                             </div>
                             <div className="mb-3">
                                 <label for="exampleFormControlTextarea1" className="form-label">Your Message</label>
@@ -78,6 +50,34 @@ const ContactUs = () => {
                             </div>
                             <button className='button2'>Send Message</button>
                         </form>
+                    </div>
+                    <div className="col-md-12 mb-5">
+                        <div class="row row-cols-1 row-cols-md-3 g-4">
+                            <div class="col">
+                                <div class="card h-100 card-background">
+                                <i className="fa-solid fa-envelope"></i>
+                                    <h5>Email</h5>
+                                    <p>admin@tangaildistrictpark.com</p>
+                                    <a href="mailto:talukderfazlerabby@gmail.com" target="_blank" rel="noreferrer">Send a message</a>
+                                </div>
+                            </div>
+                            <div class="col">
+                                <div class="card h-100 card-background">
+                                <i className="fa-brands fa-facebook-messenger"></i>
+                                    <h5>Messanger</h5>
+                                    <p>TD Park</p>
+                                    <a href="https://m.me/frtfazlerabby" target="_blank" rel="noreferrer">Send a message</a>
+                                </div>
+                            </div>
+                            <div class="col">
+                                <div class="card h-100 card-background">
+                                <i className="fa-brands fa-whatsapp"></i>
+                                    <h5>WhatsApp</h5>
+                                    <p>+8801739719796</p>
+                                    <a href="https://wa.me/8801739719796" target="_blank" rel="noreferrer">Send a message</a>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
