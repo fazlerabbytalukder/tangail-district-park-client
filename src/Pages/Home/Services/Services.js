b37eea41873524dafbc8117e067e9cd3b389fefd
fazlerabbytalukder <talukderfazlerabby@gmail.com>
2022-04-21 13:14:54 +0600

    data added

--- src/Pages/Home/Services/Services.js
+++ src/Pages/Home/Services/Services.js
@@ -14,7 +14,7 @@ const Services = () => {
             .then(res => res.json())
             .then(data => {
                 setServices(data)
-                setDisplayServices(data.slice(0,6))
+                setDisplayServices(data.slice(0,8))
                 setIsLoading(true)
             });
     }, [])
