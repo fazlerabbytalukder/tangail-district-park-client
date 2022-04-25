e77d796307f2dfe6a31f9c7f96687a5166c5be44
fazlerabbytalukder <talukderfazlerabby@gmail.com>
2022-04-25 11:58:16 +0600

    add service

--- src/Pages/Home/AddNewService/AddNewService.js
+++ src/Pages/Home/AddNewService/AddNewService.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { useForm } from 'react-hook-form';
 import './AddNewService.css';
+import addServiceImg from '../../../images/banner/addservice.jpg';
+import Header from '../../Shared/Header/Header';
 
 const AddNewService = () => {
     const { register, handleSubmit, reset, formState: { errors } } = useForm();
@@ -20,19 +22,28 @@ const AddNewService = () => {
                 }
             })
     };
-    return (  
-            <div className='d-flex justify-content-center align-items-center'>
-                <form className="register-form form-designs shadow" onSubmit={handleSubmit(onSubmit)}>
-                    <h2 className='text-center'>Add A Service</h2>
-                    <input placeholder='name' {...register("name")} />
-                    <input placeholder='ridePrice' {...register("ridePrice")} />
-                    <input placeholder='discountDay' {...register("discountDay")} />
-                    <input placeholder='rideAge' {...register("rideAge")} />
-                    <input placeholder='img link' {...register("img")} />
-                    <textarea placeholder="Description" type='textarea' className='w-100 mt-4 rounded' {...register("description")} />
-                    <input type="submit" />
-                </form>
+    return (
+        <div className="container">
+            <Header></Header>
+            <div className="row shadow">
+                <div className="col-md-6 d-flex justify-content-center align-items-center">
+                    <div className='d-flex justify-content-center align-items-center'>
+                        <form className="register-form form-designs" onSubmit={handleSubmit(onSubmit)}>
+                            <h2 className='text-center'>Add A Service</h2>
+                            <input placeholder='name' {...register("name")} />
+                            <input placeholder='Ride Price' {...register("ridePrice")} />
+                            <input placeholder='Ride Age' {...register("rideAge")} />
+                            <input placeholder='img link' {...register("img")} />
+                            <textarea placeholder="Description" type='textarea' className='w-100 mt-4 rounded' {...register("description")} />
+                            <input style={{border:'none'}} className='button-design-3' type="submit" />
+                        </form>
+                    </div>
+                </div>
+                <div className="col-md-6 d-flex justify-content-center align-items-center">
+                    <img className='img-fluid' src={addServiceImg} alt="" />
+                </div>
             </div>
+        </div>
     );
 };
 
